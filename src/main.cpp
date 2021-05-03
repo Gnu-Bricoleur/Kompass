93e00ec468516dcd303cbbc80bfa0d60f0133a6d
Sylvain Migaud <sylvain@migaud.net>
2021-05-03 21:32:49 +0200

    nothung ?

--- src/main.cpp
+++ src/main.cpp
@@ -34,64 +34,64 @@ class ApplicationData : public QObject
     Q_PROPERTY(int xraw READ xraw WRITE setXraw NOTIFY angleChanged)
     Q_PROPERTY(int yraw READ yraw WRITE setYraw NOTIFY angleChanged)
     Q_PROPERTY(int zraw READ zraw WRITE setZraw NOTIFY angleChanged)
-    public:
-        void setXraw(const int &a) {m_xraw = a;}
-        int xraw() const {return m_xraw;}
-        void setYraw(const int &a) {m_yraw = a;}
-        int yraw() const {return m_yraw;}
-        void setZraw(const int &a) {m_zraw = a;}
-        int zraw() const {return m_zraw;}
+public:
+    void setXraw(const int &a) {m_xraw = a;}
+    int xraw() const {return m_xraw;}
+    void setYraw(const int &a) {m_yraw = a;}
+    int yraw() const {return m_yraw;}
+    void setZraw(const int &a) {m_zraw = a;}
+    int zraw() const {return m_zraw;}
 
-        void setAngle(const int &a) {
-            if (a != m_angle) {
-                m_angle = a;
-                emit angleChanged();
-            }
+    void setAngle(const int &a) {
+        if (a != m_angle) {
+            m_angle = a;
+            emit angleChanged();
         }
-        int angle() const {
-            return m_angle;
-        }
-    signals:
-        void angleChanged();
-    private:
-        int m_angle;
-        int m_xraw, m_yraw, m_zraw;
+    }
+    int angle() const {
+        return m_angle;
+    }
+signals:
+    void angleChanged();
+private:
+    int m_angle;
+    int m_xraw, m_yraw, m_zraw;
 };
 
 ApplicationData data;
 
 void updateCompass(){
-	double ax, ay, az;
+    double ax, ay, az;
 
-// 	iio_channel_attr_read_double(chx, "raw", &ax);
-// 	iio_channel_attr_read_double(chy, "raw", &ay);
-// 	iio_channel_attr_read_double(chz, "raw", &az);
-//     if (ax<minx){minx = ax;}
-//     if (ax>maxx){maxx = ax;}
-//     if (ay<miny){miny = ay;}
-//     if (ay>maxy){maxy = ay;}
-// 	ax = (ax-minx)/(maxx-minx)*100;
-// 	ay = (ay-miny)/(maxy-miny)*100;
-	QVariant varx = ax;
-	QVariant vary = ay;
-	QVariant varz = az;
-	qDebug() << ax << ", " << ay << ", " << az;
-	//qmlx->setProperty("text", varx);
-	//qmly->setProperty("text", vary);
-	//qmlz->setProperty("text", varz);
-//     angle = int(atan2(int(ax), int(ay))*(180/3.14));
-// 	while(angle>360){
-// 		angle -= 360;
-// 	}
-// 	while (angle < 0){
-// 		angle += 360;
-// 	}
+    // 	iio_channel_attr_read_double(chx, "raw", &ax);
+    // 	iio_channel_attr_read_double(chy, "raw", &ay);
+    // 	iio_channel_attr_read_double(chz, "raw", &az);
+    //     if (ax<minx){minx = ax;}
+    //     if (ax>maxx){maxx = ax;}
+    //     if (ay<miny){miny = ay;}
+    //     if (ay>maxy){maxy = ay;}
+    // 	ax = (ax-minx)/(maxx-minx)*100;
+    // 	ay = (ay-miny)/(maxy-miny)*100;
+    QVariant varx = ax;
+    QVariant vary = ay;
+    QVariant varz = az;
+    qDebug() << ax << ", " << ay << ", " << az;
+    //qmlx->setProperty("text", varx);
+    //qmly->setProperty("text", vary);
+    //qmlz->setProperty("text", varz);
+    //     angle = int(atan2(int(ax), int(ay))*(180/3.14));
+    // 	while(angle>360){
+    // 		angle -= 360;
+    // 	}
+    // 	while (angle < 0){
+    // 		angle += 360;
+    // 	}
     angle += 5;
     data.setAngle(angle);
     data.setXraw(angle);
     data.setYraw(angle);
     data.setZraw(angle);
-	//kompass_hand->setProperty("rotation", angle);
+    //kompass_hand->setProperty("rotation", angle);
 }
 
 
